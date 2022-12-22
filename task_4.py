7e29b2cb475caf6c289b4b88042d057791da18d3
Maks <100966899+Lisenok17@users.noreply.github.com>
2022-12-22 12:08:53 +0300

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -21,20 +21,5 @@ conn.request('GET', d)
 r = conn.getresponse()
 parser.feed(r.read().decode())
 parser.close()
-conn = http.client.HTTPSConnection('beda.pnzgu.ru')
-print(conn)
-conn.request("GET", "/anatoly/")
-r=conn.getresponse()
-# статус ответа
-print('Proto : ', r.version)
-print('Code :', r.status)
-print('Status :', r.reason)
-# заголовки ответа
-print('---------- HEADERS ----------')
-print(r.headers)
-# тело ответа
-print('----------- BODY ------------')
-print(r.read().decode())
-conn.close()
 
 zagruzka(f)
